e50fd3b863c483c0ba03f15e4de1429e0bf86963
Anik Sarker <aniksarker1612@gmail.com>
2020-08-09 02:07:11 +0800

    call bind apply diff

--- callApply.js
+++ callApply.js
@@ -5,9 +5,9 @@ const normalPerson = {
     getFullName: function(){
         return `${this.firstName} ${this.lastName}`;
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount, tips = 0, tax = 0){
         console.log(this);
-        this.salary -= amount;
+        this.salary -= (amount + tips + tax);
         return this.salary;
     }
 }
@@ -20,27 +20,35 @@ const heroPerson = {
     firstName: 'Hero',
     lastName: 'balam',
     salary: 25000
-}
+};
 
 const heroPerson1 = {
     firstName : 'zero',
     lastName : 'golam',
     salary : 35000
-}
+};
 
 const heroPerson2 = {
     firstName : 'nero',
     lastName : 'salam',
     salary : 45000
-}
+};
 
-const heroPersonBill = normalPerson.chargeBill.bind(heroPerson);
-heroPersonBill(2000);
-heroPersonBill(2000);
-console.log(heroPerson.salary);
-console.log(normalPerson.salary);
+// const heroPersonBill = normalPerson.chargeBill.bind(heroPerson);
+// heroPersonBill(2000);
+// heroPersonBill(2000);
+// console.log(heroPerson.salary);
+// console.log(normalPerson.salary);
 
 
-const heroPerson2Bill = normalPerson.chargeBill.bind(heroPerson2);
-heroPerson2Bill(2000);
-console.log(heroPerson2.salary)
+// const heroPerson2Bill = normalPerson.chargeBill.bind(heroPerson2);
+// heroPerson2Bill(2000);
+// console.log(heroPerson2.salary);
+
+//call 
+// normalPerson.chargeBill.call(heroPerson, 1000, 100, 100);
+// console.log(heroPerson.salary);
+
+// apply
+normalPerson.chargeBill.apply(heroPerson, [3000,100,100]);
+console.log(heroPerson.salary)
